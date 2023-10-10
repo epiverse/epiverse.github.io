301daed9dc26bb164a88ab01442bcee24a3b9ee9
Jonas Almeida <jalmeida@mathbiol.org>
2023-10-10 17:00:45 -0400

    fix location reload

--- epiVerse.js
+++ epiVerse.js
@@ -1,1 +1,25 @@
-console.log('epiVerse.js loaded')
+console.log('epiVerse.js loaded');
+
+/*
+(async function(){
+    // select function
+    // console
+    let consoleArray = [
+        {
+            name:"Simple Chat",
+            parms: "url=https://episphere.github.io/gpt/export.js&ui=chatUI"
+        },
+        {
+            name:"Caterpillar",
+            parms: "https://episphere.github.io/caterpillar/caterpillar.js&ui=ui"
+        }
+    ];
+    let selConsole = document.getElementById('selConsole')
+    consoleArray.forEach(c=>{
+        let opt = document.createElement('option')
+        selConsole.appendChild(opt)
+        opt.value=(c.name)
+    })
+    
+})()
+*/
--- index.js
+++ index.js
@@ -26,7 +26,40 @@ if(location.hash.length==0){
     console.log('parms:',parms)
     // open UI in the default div
     let div = document.body.querySelector('#epiVerseDiv');
-    (await import(parms.url))[parms.ui](div)
+    // turn this into a whitelist
+    if((!!parms.url.match(/^https:\/\/episphere.github.io/)||(!!parms.url.match(/^http:\/\/localhost:8000/)))){
+        (await import(parms.url))[parms.ui](div)
+        // select function
+    // console
+    let consoleArray = [
+        {
+            name:"Simple Chat",
+            parms: "url=https://episphere.github.io/gpt/export.js&ui=chatUI"
+        },
+        {
+            name:"Caterpillar",
+            parms: "url=https://episphere.github.io/caterpillar/caterpillar.js&ui=ui"
+        }
+    ];
+    let selConsole = document.getElementById('selConsole')
+    consoleArray.forEach(c=>{
+        let opt = document.createElement('option')
+        opt.textContent=opt.value=c.name
+        selConsole.appendChild(opt)
+    })
+    selConsole.onchange=function(evt){
+        location.hash = consoleArray.filter(c=>(c.name==selConsole.value))[0].parms
+        setTimeout(location.reload,3000)
+    }
+        
+        //debugger
+            
+        //selConsole.onchange(evt=>{
+            //debugger
+        //})
+    }else{
+        alert(`origin not allowed: ${location.origin+location.pathname}`)
+    }
     //
     
 })()
